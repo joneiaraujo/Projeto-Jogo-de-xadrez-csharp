e59bbf456f2c1fdd62b99dcc6a9e3504aa91ce05
Jonei Araujo <joneispinfo@gmail.com>
2020-09-14 10:57:46 -0300

    Teste - Clonando projeto em outro computador

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -15,7 +15,7 @@ namespace xadrez_console
             }
             catch (TabuleiroException e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message); 
             }
             Console.ReadLine();
         }
